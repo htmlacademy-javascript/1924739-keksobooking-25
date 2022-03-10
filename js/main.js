0c4c0d90a000acebe8a7f7412ebd042fc4b8b94b
Andrey Dernov <andrey.dernov@gmail.com>
2022-03-10 12:17:39 +0300

    Test data generation

--- js/main.js
+++ js/main.js
@@ -14,8 +14,6 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-getRandomInt(2, 4);
-
 /**
  * @param min
  * @param max
@@ -33,4 +31,59 @@ const getRandom = (min, max, fractionalDigits) => {
   return (Math.random() * (max - min) + min).toFixed(fractionalDigits);
 };
 
-getRandom(2.43, 0, 2);
+const bookingTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkIns = ['12:00', '13:00', '14:00'];
+const checkOuts = checkIns.slice();
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const getRandomElement = (array) => array[getRandomInt(0, array.length - 1)];
+
+const getRandomString = (length) => {
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
+};
+
+const padZero = (num) => num.toString().length <= 1 ? `0${num}` : num;
+
+const bookingIds = Array.from({length: 10}, (v, i) => padZero(i + 1));
+
+const generateBooking = (id) => {
+  const location = {
+    lat: getRandom(35.65, 35.7, 5),
+    lng: getRandom(139.7, 139.8, 5)
+  };
+  const roomsNum = getRandomInt(1, 6);
+  return {
+    author: {
+      avatar: `img/avatars/user${id}.png`
+    },
+    offer: {
+      title: `Booking title ${id}`,
+      address: `${location.lat}, ${location.lng}`,
+      price: getRandomInt(5, 450),
+      type: getRandomElement(bookingTypes),
+      rooms: roomsNum,
+      guests: getRandomInt(1, roomsNum * 2),
+      checkin: getRandomElement(checkIns),
+      checkout: getRandomElement(checkOuts),
+      features: Array.from({length: getRandomInt(1, features.length - 1)}, () => getRandomElement(features)),
+      description: `Booking #${id} description`,
+      photos: Array.from({length: getRandomInt(1, 5)},
+        () => `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/${getRandomString(getRandomInt(4, 18))}-${getRandomString(getRandomInt(4, 18))}.jpg`),
+    },
+    location
+  };
+};
+
+const bookings = [];
+
+bookingIds.forEach((id) => bookings.push(generateBooking(id)));
+
+bookings.forEach((v) => {
+  // eslint-disable-next-line no-console
+  console.log(v);
+});
